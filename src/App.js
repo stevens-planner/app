520177ddf2b0e1ed23dcd52142f381505960b212
Francesca Severino <francescaseverino@Francescas-MBP.fios-router.home>
2021-04-19 15:28:34 -0400

    edits

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
+import './assets/App.css'
 
 function App() {
   return (
