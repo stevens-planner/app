be4c5f0bcdd45e7e51759a93fc507faa2740d7b7
bemin.shaker <beminshaker1@outlook.com>
2021-04-28 18:47:44 -0400

    added courseData

---
+++ src/CourseData.js
@@ -0,0 +1,170 @@
+export const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Transfer Credit",
+      cards: [
+        {
+          id: 1,
+          title: "CH115 - General Chemistry I",
+          description: 3,
+        },
+        {
+          id: 2,
+          title: "MA121: Differential Calculus",
+          description: 2,
+        },
+        {
+          id: 3,
+          title: "E120 - Engineering Graphics",
+          description: 1,
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Semester 1",
+      cards: [
+        {
+          id: 90,
+          title: "E115 - Introduction to Programming",
+          description: 2,
+        },
+        {
+          id: 677,
+          title: "E101 - Engineering Experience I",
+          description: 1,
+        },
+        {
+          id: 9999,
+          title: "E121 - Engineering Design I",
+          description: 2,
+        },
+        {
+          id: 9090,
+          title: "CAL 103 - Writing & Communications",
+          description: 3,
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Semester 2",
+      cards: [
+        {
+          id: 1000,
+          title: "PEP111 - Mechanics",
+          description: 3,
+        },
+        {
+          id: 11989,
+          title: "E122 - Engineering Design II",
+          description: 2,
+        },
+        {
+          id: 11222,
+          title: "MA123 - Series, Vectors, Functions",
+          description: 2,
+        },
+        {
+          id: 1111,
+          title: "Surfaces MA124 - Calculus of Two Variables",
+          description: 2,
+        },
+        {
+          id: 101674,
+          title: "CAL 105 - Knowledge, Nature, Culture",
+          description: 2,
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Semester 3",
+      cards: [
+        {
+          id: 120000,
+          title: "MA221 - Differential Equations",
+          description: 4,
+        },
+        {
+          id: 13888,
+          title: "E126 - Mechanics of Solids",
+          description: 4,
+        },
+        {
+          id: 11443,
+          title: "PEP112 - Electricity and Magnetism",
+          description: 3,
+        },
+        {
+          id: 1198988,
+          title: "E245 - Circuits and Systems",
+          description: 3,
+        },
+        {
+          id: 13767653,
+          title: "E231 - Engineering Design III",
+          description: 2,
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Semester 4",
+      cards: [
+        {
+          id: 120006890,
+          title: "MA134 5 - Discrete Math",
+          description: 3,
+        },
+        {
+          id: 13856898,
+          title: "E232 - Engineering Design IV",
+          description: 3,
+        },
+        {
+          id: 11984943,
+          title: "E234 - Thermodynamics",
+          description: 3,
+        },
+        {
+          id: 110989988,
+          title: "SSW215 - Individual Software Engineering",
+          description: 3,
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: "Semester 5",
+      cards: [
+        {
+          id: 1020006890,
+          title: "ISE 350 - Logistics and Supply Chain Management",
+          description: 4,
+        },
+        {
+          id: 103856898,
+          title: "E344 - Materials Processing",
+          description: 3,
+        },
+        {
+          id: 101984943,
+          title: "E321 - Engineering Design V",
+          description: 2,
+        },
+        {
+          id: 1810989988,
+          title: "E243 - Probability & Statistics",
+          description: 3,
+        },
+        {
+          id: 1810989988,
+          title: "SSW 315 - Object-based Software Development",
+          description: 3,
+        },
+      ],
+    },
+  ],
+};
--- src/DragComponent.js
+++ src/DragComponent.js
@@ -1,78 +1,10 @@
 import React, { useState } from "react";
 import Board, { moveCard } from "@lourenci/react-kanban";
+import { board } from "./CourseData";
 import "./DragComponent.css";
 import firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
-
-const board = {
-  columns: [
-    {
-      id: 1,
-      title: "Transfer Credit",
-      cards: [
-        {
-          id: 1,
-          title: "CH115 - General Chemistry I",
-          description: "Card content",
-        },
-        {
-          id: 2,
-          title: "MA121: Differential Calculus",
-          description: "Card content",
-        },
-        {
-          id: 3,
-          title: "E120 - Engineering Graphics",
-          description: "Card content",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "Semester 1",
-      cards: [
-        {
-          id: 9,
-          title: "E115 - Introduction to Programming",
-          description: "Card content",
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: "Semester 2",
-      cards: [
-        {
-          id: 10,
-          title: "PEP111 - Mechanics",
-          description: "Card content",
-        },
-        {
-          id: 11,
-          title: "E122 - Engineering Design II",
-          description: "Card content",
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "Semester 3",
-      cards: [
-        {
-          id: 12,
-          title: "MA221 - Differential Equations",
-          description: "Card content",
-        },
-        {
-          id: 13,
-          title: "E126 - Mechanics of Solids",
-          description: "Card content",
-        },
-      ],
-    },
-  ],
-};
 
 function ControlledBoard() {
   const [controlledBoard, setBoard] = useState(board);
@@ -105,8 +37,9 @@ function ControlledBoard() {
   return (
     <div>
       <h1>Stevens Study Planner</h1>
-      <Board onCardDragEnd={handleCardMove}>{controlledBoard}</Board>
       <button onClick={updateBoard}>Update</button>
+      <Board onCardDragEnd={handleCardMove}>{controlledBoard}</Board>
+      {/*<button onClick={setInitialBoard}>Send</button>*/}
     </div>
   );
 }
